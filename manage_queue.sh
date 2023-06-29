19d1053482f197e755ecc4ec1826bbac07578bb0
Yangfan Wu <1921193637@qq.com>
2023-06-29 10:20:33 +0800

    first commit

---
+++ manage_queue.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+queue_file="training_queue.txt"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+GRAY='\033[1;30m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+
+function print_queue {
+  while read -r task; do
+    IFS=',' read -r -a task_info <<< "$task"
+    priority=${task_info[0]}
+    id=${task_info[1]}
+    script_path=${task_info[2]}
+    num_gpus=${task_info[3]}
+    pid=${task_info[4]}
+
+    if [ "$pid" == "-1" ]; then
+      status="${WHITE}Idle${NC}"
+    elif [ "$pid" == "-2" ]; then
+      status="${GRAY}Completed${NC}"
+    elif [ "$pid" == "-3" ]; then
+      status="${RED}Failed${NC}"
+    else
+      if ps -p $pid > /dev/null; then
+        status="${GREEN}Running${NC}"
+      fi
+    fi
+
+    echo -e "Priority: $priority, Task ID: $id, Script Path: $script_path, GPUs Required: $num_gpus, PID: $pid, Status: $status"
+  done < <(sort -t',' -k1rn $queue_file) # Print in descending order of priority
+}
+
+while true; do
+  echo "Please choose an action:"
+  echo "1. Add a task"
+  echo "2. Remove a task"
+  echo "3. Kill a task"
+  echo "4. Change task priority"
+  echo "5. Print the queue"
+  echo "6. Renew a task"
+  echo "7. Exit"
+
+  read action
+
+  case $action in
+    1)
+      echo "Enter the task script path:"
+      read script_path
+      echo "Enter the number of GPUs required:"
+      read num_gpus
+      id=$(uuidgen)
+      max_priority=$(cut -d',' -f1 $queue_file | sort -rn | head -n1)
+      new_priority=$((max_priority + 1))
+      echo "$new_priority,$id,$script_path,$num_gpus,-1" >> $queue_file
+      ;;
+    2)
+      echo "Enter the task ID to remove:"
+      read task_id
+      sed -i "/,$task_id,/d" $queue_file
+      ;;
+    3)
+      echo "Enter the task ID to kill:"
+      read task_id
+      pid=$(awk -F',' "/,$task_id,/ { print \$5 }" $queue_file)
+      if [ "$pid" != "-1" ] && [ "$pid" != "-2" ] && [ "$pid" != "-3" ]; then
+        kill $pid
+        sed -i "/,${task_id},/ s/[^,]*$/-3/" $queue_file
+      fi
+      ;;
+    4)
+      echo "Enter the task ID to change priority:"
+      read task_id
+      echo "Enter the new priority:"
+      read new_priority
+      sed -i "/,${task_id},/ s/^[^,]*,/${new_priority},/" $queue_file
+      ;;
+    5)
+      print_queue
+      ;;
+    6)
+      echo "Enter the task ID to renew:"
+      read task_id
+      while IFS=',' read -r priority id script_path num_gpus pid; do
+        if [ "$id" == "$task_id" ]; then
+          echo "$priority,$id,$script_path,$num_gpus,-1" >> $queue_file.tmp
+        else
+          echo "$priority,$id,$script_path,$num_gpus,$pid" >> $queue_file.tmp
+        fi
+      done < $queue_file
+      mv $queue_file.tmp $queue_file
+      ;;
+    7)
+      exit 0
+      ;;
+    *)
+      echo "Invalid action. Please enter a number from 1 to 7."
+      ;;
+  esac
+done
---
+++ monitor_and_train.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+queue_file="training_queue.txt"
+log_file="log/monitor_and_train.log"
+
+if [ ! -d "./log" ]; then
+  mkdir ./log
+fi
+
+function run_task {
+  task_line=$1
+  IFS=',' read -r -a task_info <<< "$task_line"
+  priority=${task_info[0]}
+  id=${task_info[1]}
+  script_path=${task_info[2]}
+  num_gpus=${task_info[3]}
+  pid=${task_info[4]}
+
+  if [ "$pid" == "-1" ]; then
+    timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+    available_gpus=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits | awk '$1 <= 300 { print NR-1 }')
+    gpu_ids=$(echo $available_gpus | tr ' ' '\n' | head -n $num_gpus | tr '\n' ',')
+    gpu_ids=${gpu_ids%?} # remove trailing comma
+    echo "$timestamp: Starting task $id with GPU $gpu_ids" >> $log_file
+    {
+      CUDA_VISIBLE_DEVICES=$gpu_ids /bin/bash $script_path && echo "Task completed successfully"
+    } > "log/task_${id}_log.txt" 2>&1 &  # Redirect task output to a log file
+    new_pid=$(pgrep -f $script_path | xargs)
+    sed -i "s|^$priority,$id,.*$|$priority,$id,$script_path,$num_gpus,$new_pid|" $queue_file # Update PID in queue file
+  fi
+}
+
+
+function update_task_status {
+  task_line=$1
+  IFS=',' read -r -a task_info <<< "$task_line"
+  priority=${task_info[0]}
+  id=${task_info[1]}
+  script_path=${task_info[2]}
+  num_gpus=${task_info[3]}
+  pid=${task_info[4]}
+
+  if [ -n "$pid" ] && [ "$pid" -eq "$pid" ] 2>/dev/null; then
+    if [ "$pid" != "-1" ] && [ "$pid" != "-2" ] && [ "$pid" != "-3" ]; then
+      if ! ps -p $pid > /dev/null; then
+        # Check the last line of the task's log file to determine success or failure
+        if tail -n 2 "log/task_${id}_log.txt" | grep -q "Task completed successfully"; then
+          new_pid="-2"  # Successful completion
+          timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+          echo "$timestamp: Task $id completed successfully" >> $log_file
+        else
+          new_pid="-3"  # Task failure
+          timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+          echo "$timestamp: Task $id failed" >> $log_file
+        fi
+        sed -i "s|^$priority,$id,.*$|$priority,$id,$script_path,$num_gpus,$new_pid|" $queue_file # Update PID in queue file
+      fi
+    fi
+  fi
+}
+
+
+while true; do
+  timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+  available_gpus=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits | awk -F ',' '{ if ($1<=300) print $1 }' | wc -l)
+  echo "$timestamp: Checking GPU status, Aviable GPUs: $available_gpus" >> $log_file
+  
+  echo "$timestamp: Checking GPU status, Aviable GPUs: $available_gpus"
+
+  # Process tasks in descending order of priority
+  sort -t',' -k1rn $queue_file | while read -r task; do
+    num_gpus_required=$(echo $task | cut -d',' -f4)
+    pid=$(echo $task | cut -d',' -f5)
+
+    update_task_status $task
+    
+    # Only run the task if its PID is -1 (idle)
+    if [ "$pid" == "-1" ] && [ "$available_gpus" -ge "$num_gpus_required" ]; then
+      echo "Start running task $task"
+      run_task $task
+      available_gpus=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits | awk -F ',' '{ if ($1<=300) print $1 }' | wc -l)
+    fi
+  done
+
+  sleep 30
+done
